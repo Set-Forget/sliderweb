cf7419008db467c7fab7e85c9cf21403b74f9d9d
Agustin <agustindariodelgado@gmail.com>
2024-01-12 17:38:45 -0300

    Added demo gif

--- app/components/Demo.js
+++ app/components/Demo.js
@@ -1,17 +1,16 @@
-import Image from 'next/image'
-import demoImg from '@/public/SliderWebDemo.png'
+import sliderWebGif from "@/public/slider-web-gif.gif";
+import Image from 'next/image';
 
 export const Demo = () => {
     return (
         <div className="relative isolate dark:bg-gray-900 flex">
-            <div className="mx-auto px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40 max-w-[1366px]">
+            <div className="mx-auto items-center gap-14 px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40 max-w-[1366px]">
                 <div>
-                    <Image src={demoImg} width={0} alt='sliderweb usage demo'/>
+                    <Image className='rounded-lg shadow-lg' src={sliderWebGif} width={0} alt='sliderweb usage demo' />
                 </div>
                 <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
                     <h2 className="text-4xl font-bold tracking-tight sm:text-6xl mt-6">Import your templates wherever and whenever</h2>
-                    <p className="text-lg leading-8 dark:text-gray-300 mt-16">
-                        Efficiently create and import custom slide templates into any presentation, streamlining
+                    <p className="text-lg leading-8 dark:text-gray-300 mt-6">Efficiently create and import custom slide templates into any presentation, streamlining
                         your slide creation process.
                     </p>
                     <p className='text-lg leading-8 dark:text-gray-300 mt-6'>
